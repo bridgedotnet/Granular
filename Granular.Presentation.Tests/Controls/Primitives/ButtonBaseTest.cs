834c2cf79d46ef52656a9fc187441011d97b3119
Miha Markič <miha@rthand.com>
2015-11-07 09:51:22 +0100

    removed Moq, refactored tests to run without Moq

--- Granular.Presentation.Tests/Controls/Primitives/ButtonBaseTest.cs
+++ Granular.Presentation.Tests/Controls/Primitives/ButtonBaseTest.cs
@@ -1,38 +1,23 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls.Primitives;
-using System.Windows.Input;
 
 namespace Granular.Presentation.Tests.Controls.Primitives
 {
     [TestClass]
     public class ButtonBaseTest
     {
-        protected ButtonBase target;
-
-        [TestInitialize]
-        public void SetUp()
-        {
-            target = new ButtonBase();
-        }
-
         [TestClass]
-        public class Command: ButtonBaseTest
+        public class Command
         {
             [TestMethod]
             public void WhenAttachingNewCommand_OldIsDetached()
             {
+                ButtonBase target = new ButtonBase();
                 bool canExecuteCalled;
                 RelayCommand firstCommand = new RelayCommand(p => { }, p => true);
                 target.Command = firstCommand;
-                Mock<ICommand> secondCommand = new Mock<ICommand>();
-                secondCommand.Setup(mq => mq.CanExecute(It.IsAny<object>())).Returns(() => { canExecuteCalled = true; return false; });
-                target.Command = secondCommand.Object;
+                RelayCommand secondCommand = new RelayCommand(p => { }, p => { canExecuteCalled = true; return false; });
+                target.Command = secondCommand;
 
                 canExecuteCalled = false;
                 // call to detached ICommand shouldn't trigger CanExecute changed on ButtonBase
@@ -44,6 +29,7 @@ namespace Granular.Presentation.Tests.Controls.Primitives
             [TestMethod]
             public void AfterAssigningCommandThatDisables_IsEnabledIsFalse()
             {
+                ButtonBase target = new ButtonBase();
                 RelayCommand command = new RelayCommand(p => { }, p => false);
                 target.Command = command;
 
@@ -52,6 +38,7 @@ namespace Granular.Presentation.Tests.Controls.Primitives
             [TestMethod]
             public void AfterAssigningCommandThatEnables_IsEnabledIsTrue()
             {
+                ButtonBase target = new ButtonBase();
                 RelayCommand command = new RelayCommand(p => { }, p => true);
                 target.Command = command;
 
@@ -60,6 +47,7 @@ namespace Granular.Presentation.Tests.Controls.Primitives
             [TestMethod]
             public void CommandCanExecuteChanged_IsHandled()
             {
+                ButtonBase target = new ButtonBase();
                 bool isEnabled = false;
                 RelayCommand command = new RelayCommand(p => { }, p => isEnabled);
                 target.Command = command;
@@ -73,11 +61,13 @@ namespace Granular.Presentation.Tests.Controls.Primitives
             [TestMethod]
             public void WhenCommandParameterIsAssignedAndCommandIsAssigned_CommandParameterIsPassedToCanExecute()
             {
+                ButtonBase target = new ButtonBase();
+                bool canExecuteCalled = false;
                 target.CommandParameter = 57;
-                Mock<ICommand> command = new Mock<ICommand>();
-                target.Command = command.Object;
+                RelayCommand command = new RelayCommand(p => { }, p => canExecuteCalled = true);
+                target.Command = command;
 
-                command.Verify(mq => mq.CanExecute(It.Is<object>(o => (int)o == 57)), Times.Once);
+                Assert.IsTrue(canExecuteCalled);
             }
         }
     }
