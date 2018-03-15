b2dc4c918364f68cb4ee9d3e583cfe025967a3d3
Geoffrey McGill <geoff@object.net>
2018-03-15 00:22:56 -0600

    Upgrade to Bridge 16.8.2

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,28 +1,8 @@
 ﻿using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Granular")]
-[assembly: AssemblyCopyright("Copyright ☺ 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("0.3.0.0")]
-
-[assembly: AssemblyFileVersion("0.3.0.0")]
-[assembly: AssemblyInformationalVersion("0.3.0.0")]
+[assembly: AssemblyCompany("Object.NET, Inc.")]
+[assembly: AssemblyCopyright("Copyright 2008-2018 Object.NET, Inc.")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyInformationalVersion("0.4.0.0")]
